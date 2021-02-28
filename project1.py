6e18c317998b0ca7b85e0b092f97ed2a70b0becb
scottsak <48599109+scottsak@users.noreply.github.com>
2021-02-28 00:41:44 -0800

    code

---
+++ project1.py
@@ -0,0 +1,17 @@
+'''
+EE 281 Project 1 - Part 1
+Scott Sakurai
+018497139
+'''
+import random
+
+p = float(input('Enter the probability of success: '))
+T = int(input('Enter the probability of trials: '))
+
+for i in range(T):
+    r = random.uniform(0,1)
+
+    if r<p:
+        print('1', end=' ')
+    else:
+        print('0',end=' ')
